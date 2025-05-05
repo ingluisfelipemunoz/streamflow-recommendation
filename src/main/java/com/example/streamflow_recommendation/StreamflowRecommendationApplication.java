402be860941cf23162476b1381640560928fbbe3
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2025-05-04 22:06:53 -0400

    Initialize Spring Boot project with basic structure and configuration files

---
+++ src/main/java/com/example/streamflow_recommendation/StreamflowRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamflow_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamflowRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamflowRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/streamflow_recommendation/StreamflowRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamflow_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamflowRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
